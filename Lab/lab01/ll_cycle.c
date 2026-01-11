6928cbe76d3d4607b629c9a7449836713b3099de
zz <1924293797@qq.com>
2026-01-11 17:00:11 +0800

    finish lab0 and lab1

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -2,6 +2,24 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+    node* turtle = head;
+    if(head == NULL){
+        return 0;
+    }
+    node* rabbit = head->next;
+
+    while(turtle!=rabbit){
+        if(rabbit->next==NULL){  //nullptr
+            return 0;
+        }
+
+        rabbit=rabbit->next->next;
+
+        if(rabbit==NULL){
+            return 0;
+        }
+        turtle = turtle->next;
+
+    }
+    return 1;
 }
---
+++ Lab/lab01/program.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("张泽大帅逼,张艺洋小丑");
+    return 0;
+}
+
