2fd7d3249f0e896bd8951fb838b2b08f4456e32c
zz <1924293797@qq.com>
2026-06-10 19:48:17 +0800

    finish lab09

--- Lab/lab09/simd.c
+++ Lab/lab09/simd.c
@@ -56,6 +56,30 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 
 		/* You'll need a tail case. */
 
+		__m128i temp = _mm_setzero_si128(); 
+		
+		for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+			__m128i vec = _mm_loadu_si128((__m128i*)(vals + i)); // Load 4 integers into a 128-bit vector 
+			__m128i mask = _mm_cmpgt_epi32(vec, _127); // Compare each element with 127, creating a mask 
+
+			__m128i filtered_vec = _mm_and_si128(vec, mask); // Zero out elements that are not greater than 127
+			temp = _mm_add_epi32(temp, filtered_vec); // Accumulate the sum in temp
+		}
+
+		for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
+
+		// store __mm128i result into int array 
+		int temp_array[4];
+		_mm_storeu_si128((__m128i*)temp_array, temp);
+
+		// sum up the elements in temp_array and add to result 
+		for(int j = 0; j < 4; j++) {
+			result += temp_array[j];
+		}
 	}
 	clock_t end = clock();
 	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
@@ -71,6 +95,51 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
 		/* MODIFY IT BY UNROLLING IT */
 
 		/* You'll need 1 or maybe 2 tail cases here. */
+		__m128i temp1 = _mm_setzero_si128();
+		__m128i temp2 = _mm_setzero_si128();
+		__m128i temp3 = _mm_setzero_si128();
+		__m128i temp4 = _mm_setzero_si128();
+
+		for(unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+			__m128i vec1 = _mm_loadu_si128((__m128i*)(vals + i)); // Load first 4 integers
+			__m128i vec2 = _mm_loadu_si128((__m128i*)(vals + i + 4)); // Load next 4 integers
+			__m128i vec3 = _mm_loadu_si128((__m128i*)(vals + i + 8)); // Load next 4 integers
+			__m128i vec4 = _mm_loadu_si128((__m128i*)(vals + i + 12)); // Load next 4 integers
+
+			__m128i mask1 = _mm_cmpgt_epi32(vec1, _127); // Compare first vector with 127
+			__m128i mask2 = _mm_cmpgt_epi32(vec2, _127); // Compare second vector with 127
+			__m128i mask3 = _mm_cmpgt_epi32(vec3, _127); // Compare third vector with 127
+			__m128i mask4 = _mm_cmpgt_epi32(vec4, _127); // Compare fourth vector with 127
+
+			__m128i filtered_vec1 = _mm_and_si128(vec1, mask1); // Filter first vector
+			__m128i filtered_vec2 = _mm_and_si128(vec2, mask2); // Filter second vector
+			__m128i filtered_vec3 = _mm_and_si128(vec3, mask3); // Filter third vector
+			__m128i filtered_vec4 = _mm_and_si128(vec4, mask4); // Filter fourth vector
+
+			temp1 = _mm_add_epi32(temp1, filtered_vec1); // Accumulate first vector
+			temp2 = _mm_add_epi32(temp2, filtered_vec2); // Accumulate second vector
+			temp3 = _mm_add_epi32(temp3, filtered_vec3); // Accumulate third vector
+			temp4 = _mm_add_epi32(temp4, filtered_vec4); // Accumulate fourth vector
+		}
+
+		for(unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				result += vals[i];
+			}
+		}
+
+		int temp_array1[4], temp_array2[4], temp_array3[4], temp_array4[4];
+		_mm_storeu_si128((__m128i*)temp_array1, temp1); // Store the first vector
+		_mm_storeu_si128((__m128i*)temp_array2, temp2); // Store the second vector
+		_mm_storeu_si128((__m128i*)temp_array3, temp3); // Store the third vector
+		_mm_storeu_si128((__m128i*)temp_array4, temp4); // Store the last vector
+
+		for(int j = 0; j < 4; j++) {
+			result += temp_array1[j];
+			result += temp_array2[j];
+			result += temp_array3[j];
+			result += temp_array4[j];
+		}
 
 	}
 	clock_t end = clock();
